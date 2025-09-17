793599ceefee81f59b775cd67031d8718ecba6d6
Peter Emu <emu.kimani@strathmore.edu>
2025-09-17 22:00:21 +0300

    few changes

---
+++ README.md
@@ -0,0 +1,9 @@
+Project: Fetch and Display Content from db.json
+
+Author: Peter Emu Kimani
+
+ Description
+This project demonstrates how to fetch data from a db.json file, process it using a JavaScript (.js) file, and display the content dynamically in a trial .html file.
+
+It is a simple project aimed at practicing JSON handling, API simulation, and DOM manipulation.
+
--- survey.js
+++ survey.js
@@ -1,5 +1,8 @@
-const submition=document.getElementById("submitBtn")
-submition.addEventListener('submit', function(idk){
-    idk.preventDefault()
-}
-)
+const form = document.getElementById("surveyForm");
+form.addEventListener("submit", function (event) {
+                event.preventDefault();
+})
+submitBtn.addEventListener("click", () => {
+  console.log("your form has been submitted");
+  alert("form submitted");
+});
